bdfc6153977eb23521a367ba2560c3855027d87d
Simon Skrippek <simon.skrippek@t-online.de>
2021-08-26 00:56:21 +0200

    finished-ish initiali gameplay setup

---
+++ Assets/Whack-A-Stoodent/Runtime/Helper/Gizmo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    [Range(.1f,10f)]
+    [SerializeField] private float size;
+    [ColorUsage(true)]
+    [SerializeField] private Color color = Color.yellow;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = this.color;
+        Gizmos.DrawWireSphere(this.transform.position, this.size);
+    }
+}
---
+++ Assets/Whack-A-Stoodent/Runtime/InGame/InGameInputReceiver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace WhackAStoodent.InGame
+{
+    public class InGameInputReceiver : MonoBehaviour
+    {
+        [SerializeField] private Camera _camera;
+        [SerializeField] private LayerMask _targetLayerMask;
+        
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                ProcessClick();
+            }
+        }
+
+        private void ProcessClick()
+        {
+            Vector3 position = Input.mousePosition;
+            var ray = _camera.ScreenPointToRay(position, Camera.MonoOrStereoscopicEye.Mono);
+            
+            //Debug.Log($"Ray from {ray.origin} in direction {ray.direction}");
+            Debug.DrawRay(ray.origin, ray.direction);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, 10f, _targetLayerMask, QueryTriggerInteraction.Collide))
+            {
+                Debug.Log($"hit target {hit.collider.gameObject.name} at {(Vector2)hit.point}");
+            }
+        }
+    }
+}
